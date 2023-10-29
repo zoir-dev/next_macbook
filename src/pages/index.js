a8fe90aef0d0c3c04c3e34568994ee4876c6b3c7
Zoirjon <zoirjonabduvohidov0@gmail.com>
2023-10-29 21:07:45 +0500

    facebook metrika

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 "use client";
-import YandexMetrika from "next-yandex-metrika";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 import Get from "../components/Get/Get";
@@ -9,6 +8,7 @@ import Portfolio from "../components/Portfolio/Portfolio";
 import Work from "../components/Work/Work";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
+import { Helmet } from "react-helmet";
 export default function Home() {
   const scrollToComponent = (id) => {
     const targetComponent = document.getElementById(id);
@@ -24,13 +24,50 @@ export default function Home() {
       <Head>
         <title>{metadata.title}</title>
         <link rel="icon" href={metadata.image} type="image/png" sizes="16x16" />
-        <YandexMetrika
-          yid="95384978"
-          clickmap={true}
-          trackLinks={true}
-          accurateTrackBounce={true}
-          webvisor={true}
-        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          
+          ym(95384978, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+          })`,
+          }}
+        ></script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/95384978"
+              style="position:absolute; left:-9999px;"
+              alt=""
+            />
+          </div>
+        </noscript>
+        <Helmet>
+          {`<script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '871193114731707');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=871193114731707&ev=PageView&noscript=1"
+            /></noscript>
+          `}
+        </Helmet>
       </Head>
       <Header scrollToComponent={scrollToComponent} />
       <div className="main_div">
