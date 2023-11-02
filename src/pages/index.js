f94d6946f99d35d7fa7ff6167b285ce8620291e8
Zoirjon <zoirjonabduvohidov0@gmail.com>
2023-11-02 12:39:34 +0500

    facebook meta edited

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,24 +25,23 @@ export default function Home() {
       <Head>
         <title>{metadata.title}</title>
         <link rel="icon" href={metadata.image} type="image/png" sizes="16x16" />
-
         <Helmet>
           {`
-            <script>
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '871193114731707');
-              fbq('track', 'PageView');
-              </script>
-              <noscript><img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=871193114731707&ev=PageView&noscript=1"
-              /></noscript>
+           <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '727650709182274');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=727650709182274&ev=PageView&noscript=1"
+            /></noscript>
             `}
         </Helmet>
       </Head>
