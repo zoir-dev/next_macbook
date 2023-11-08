e12b8dfaabe395e5eccc471b5e93f1c7c2303b50
Zoirjon <zoirjonabduvohidov0@gmail.com>
2023-11-08 07:39:44 +0500

    yandex worked

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,16 +28,18 @@ export default function Home() {
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
-           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
             m[i].l=1*new Date();
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(95499691, "init", {
+            ym(86849414, "init", {
                   clickmap:true,
                   trackLinks:true,
-                  accurateTrackBounce:true
+                  accurateTrackBounce:true,
+                  webvisor:true,
+                  ecommerce:"dataLayer"
             });
                 `,
           }}
@@ -45,7 +47,7 @@ export default function Home() {
         <noscript>
           <div>
             <img
-              src="https://mc.yandex.ru/watch/95499691"
+              src="https://mc.yandex.ru/watch/86849414"
               style={{ position: "absolute", left: "-9999px" }}
               alt=""
             />
